e848121da014459e9944c6a104f962f38aa389a0
Neemah-Aliyu <sofiyaaliyu989@gmail.com>
2018-04-14 14:14:46 +0100

    Add files via upload

---
+++ hotels.sofiya/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TickTock</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="center">
+	<h1>TICK TOCK!</h1>
+<h1><?php echo date("h:i:a"); ?></h1>
+	<h3>says the clock</h3>
+</div>
+</body>
+</html>
